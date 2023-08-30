e6be830138d354e519276176ce1b2df1112b249b
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-08-30 13:48:51 +0200

    Creating api

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello, World');
+})
+
+app.get('/users', (req, res) => {
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+        }
+        const realData = JSON.parse(data).users;
+        res.send(realData);
+    })
+})
+
+app.get('/users/:userId', (req, res) => {
+    fs.readFile('./users.json', 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+        }
+
+        const users = JSON.parse(data).users;
+        const userId = Number(req.params.userId);
+
+        const user = users.find(user => user.id === userId);
+
+        if(user){
+            res.send(user);
+        } else{
+            res.status(404);
+            res.send({state: 'User not found!'});
+        }
+    })
 })
 
 app.listen(3000, () => {
